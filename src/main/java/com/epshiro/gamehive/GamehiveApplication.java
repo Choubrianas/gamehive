0f332ebc38a97908d6fb8e147264f4f02ce1dbdb
choubrianas <choubrianas@gmail.com>
2024-01-03 01:25:01 +0100

    initial commit with working liquibase migration

---
+++ src/main/java/com/epshiro/gamehive/GamehiveApplication.java
@@ -0,0 +1,13 @@
+package com.epshiro.gamehive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamehiveApplication.class, args);
+    }
+
+}
